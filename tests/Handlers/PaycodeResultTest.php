1b72458ae8e00fcdcb97fc4ae06db9e84d4526e6
Vincent Chi <song374561@chivincent.net>
2019-12-05 13:46:10 +0800

    test: create test case for paycode payment handler.

---
+++ tests/Handlers/PaycodeResultTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Test\Handlers;
+
+use Muzik\EsafeSdk\Foundation\Testing\Faker;
+use Muzik\EsafeSdk\Handlers\PaycodeResult;
+use PHPUnit\Framework\TestCase;
+
+/*
+ * Notice: This test case is not mocked by esafe.com.tw responses.
+ */
+class PaycodeResultTest extends TestCase
+{
+    use Faker;
+
+    protected $parameters = [
+        'buysafeno' => '2400009912300000019',
+        'web' => 'S1103020010',
+        'Td' => '',
+        'MN' => '1000',
+        'Name' => 'V****** **i',
+        'note1' => '',
+        'note2' => '',
+        'UserNo' => '',
+        'PayDate' => '20200101',
+        'PayType' => '5',
+        'PayAgency' => '',
+        'PayAgencyName' => '',
+        'PayAgencyTel' => '',
+        'PayAgencyAddress' => '',
+        'errcode' => '00',
+        'CargoNo' => '',
+        'StoreID' => '',
+        'StoreName' => '',
+        'InvoiceNo' => '',
+        'ChkValue' => '6E0ED343525CDCBE678BB1103054CBA25E634282',
+    ];
+
+    public function test_constructable()
+    {
+        $this->assertInstanceOf(PaycodeResult::class, new PaycodeResult($this->makeRequest($this->parameters), 'abcd5888'));
+    }
+}
---
+++ tests/Handlers/PaycodeTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Test\Handlers;
+
+use Muzik\EsafeSdk\Foundation\Testing\Faker;
+use Muzik\EsafeSdk\Handlers\Paycode;
+use PHPUnit\Framework\TestCase;
+
+class PaycodeTest extends TestCase
+{
+    use Faker;
+
+    protected $parameters = [
+        'buysafeno' =>	'2400009912300000019',
+        'web' =>	'S1103020010',
+        'MN' =>	'1000',
+        'Td' =>	'',
+        'webname' =>	'香港商帕格數碼媒體股份有限公司',
+        'Name' =>	'V****** **i',
+        'note1' =>	'',
+        'note2' =>	'',
+        'ApproveCode' =>	'',
+        'Card_NO' =>	'',
+        'Card_Type' =>	'',
+        'UserNo' =>	'',
+        'PayDate' =>	'',
+        'PayTime' =>	'',
+        'SendType' =>	'1',
+        'errcode' =>	'',
+        'errmsg' =>	'',
+        'paycode' =>	'LAC90824000098',
+        'PayType' =>	'4,5,6,7',
+        'PayAgency' =>	'',
+        'PayAgencyMemo' =>	'',
+        'PayAgencyName' =>	'',
+        'PayAgencyTel' =>	'',
+        'PayAgencyAddress' =>	'',
+        'BarcodeA' =>	'',
+        'BarcodeB' =>	'',
+        'BarcodeC' =>	'',
+        'PostBarcodeA' =>	'',
+        'PostBarcodeB' =>	'',
+        'PostBarcodeC' =>	'',
+        'EntityATM' =>	'',
+        'BankCode' =>	'',
+        'BankName' =>	'',
+        'CargoNo' =>	'',
+        'StoreName' =>	'',
+        'StoreID' =>	'',
+        'InvoiceNo' =>	'',
+        'tokenData' =>	'',
+        'ChkValue' =>	'9C53A993836A12DD477CF39FBB10E0C4E67323E0',
+    ];
+
+    public function test_constructable()
+    {
+        $this->assertInstanceOf(Paycode::class, new Paycode($this->makeRequest($this->parameters), 'abcd5888'));
+    }
+}
