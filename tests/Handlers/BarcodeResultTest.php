3ee920a008336c199fa863d1ae4e8787be118230
Vincent Chi <song374561@chivincent.net>
2019-12-05 13:14:55 +0800

    test: create test case for barcode payment handler.

---
+++ tests/Handlers/BarcodeResultTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Test\Handlers;
+
+use Muzik\EsafeSdk\Foundation\Testing\Faker;
+use Muzik\EsafeSdk\Handlers\BarcodeResult;
+use PHPUnit\Framework\TestCase;
+
+class BarcodeResultTest extends TestCase
+{
+    use Faker;
+
+    protected $parameters = [
+        'buysafeno' =>	'2400009912300000019',
+        'web' =>	'S1103020010',
+        'MN' =>	'1000',
+        'Td' =>	'',
+        'webname' => '英屬維京群島商希幔數位有限公司台灣分公司',
+        'Name' =>	'V○○○○○○ ○○i',
+        'note1' =>	'',
+        'note2' =>	'',
+        'ApproveCode' =>	'',
+        'Card_NO' =>	'',
+        'Card_Type' =>	'',
+        'UserNo' =>	'',
+        'PayDate' =>	'',
+        'PayTime' =>	'',
+        'SendType' =>	'1',
+        'errcode' =>	'00',
+        'errmsg' =>	'交易成功',
+        'PayType' =>	'',
+        'PayAgency' =>	'',
+        'PayAgencyMemo' =>	'',
+        'PayAgencyName' =>	'',
+        'PayAgencyTel' =>	'',
+        'PayAgencyAddress' =>	'',
+        'CargoNo' =>	'',
+        'StoreName' =>	'',
+        'StoreID' =>	'',
+        'InvoiceNo' =>	'',
+        'tokenData' =>	'',
+        'ChkValue' =>	'6E0ED343525CDCBE678BB1103054CBA25E634282',
+    ];
+
+    public function test_constructable()
+    {
+        $this->assertInstanceOf(BarcodeResult::class, new BarcodeResult($this->makeRequest($this->parameters), 'abcd5888'));
+    }
+}
