930073fb594004f6f23bee0a451098387bd4b09f
Vincent Chi <song374561@chivincent.net>
2019-12-05 11:51:09 +0800

    feat: construct all of handlers from entrypoint.

--- src/Handlers/BaseHandler.php
+++ src/Handlers/BaseHandler.php
@@ -3,7 +3,11 @@
 namespace Muzik\EsafeSdk\Handlers;
 
 use Muzik\EsafeSdk\Contracts\Handler;
+use Psr\Http\Message\ServerRequestInterface;
 
 abstract class BaseHandler implements Handler
 {
+    public function __construct(ServerRequestInterface $request, string $apiKey)
+    {
+    }
 }
