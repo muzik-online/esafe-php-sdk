2ce50e5b5504c4ff9cbae0a37a1ad7096507cb08
Vincent Chi <song374561@chivincent.net>
2019-12-05 12:42:08 +0800

    feat: construct handlers and store parameter and api key.

--- src/Handlers/BaseHandler.php
+++ src/Handlers/BaseHandler.php
@@ -7,7 +7,23 @@ use Psr\Http\Message\ServerRequestInterface;
 
 abstract class BaseHandler implements Handler
 {
+    /**
+     * The parameters requested from esafe.com.tw.
+     *
+     * @var array
+     */
+    protected array $parameters;
+
+    /**
+     * The transaction password initialized by entrypoint.
+     *
+     * @var string
+     */
+    protected string $apiKey;
+
     public function __construct(ServerRequestInterface $request, string $apiKey)
     {
+        $this->parameters = array_filter((array) $request->getParsedBody(), fn ($item) => $item !== '');
+        $this->apiKey = $apiKey;
     }
 }
