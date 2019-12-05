8cdb1e38e21bb00ce60b63cf75e9c67ba1c3fe47
Vincent Chi <song374561@chivincent.net>
2019-12-05 13:07:05 +0800

    test: create test case for bank transfer payment handler.

---
+++ tests/Handlers/BankTransferTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Test\Handlers;
+
+use Muzik\EsafeSdk\Foundation\Testing\Faker;
+use Muzik\EsafeSdk\Handlers\BankTransfer;
+use PHPUnit\Framework\TestCase;
+
+class BankTransferTest extends TestCase
+{
+    use Faker;
+
+    protected $parameters = [
+        'buysafeno' => '2400009912300000019',
+        'web' => 'S1103020010',
+        'MN' => '1000',
+        'Td' => '',
+        'webname' => '英屬維京群島商希幔數位有限公司台灣分公司',
+        'Name' => 'V****** **i',
+        'note1' => '',
+        'note2' => '',
+        'ApproveCode' => '',
+        'Card_NO' => '',
+        'Card_Type' => '',
+        'UserNo' => '',
+        'PayDate' => '',
+        'PayTime' => '',
+        'SendType' => '1',
+        'errcode' => '',
+        'errmsg' => '',
+        'paycode' => '',
+        'PayType' => '',
+        'PayAgency' => '',
+        'PayAgencyMemo' => '',
+        'PayAgencyName' => '',
+        'PayAgencyTel' => '',
+        'PayAgencyAddress' => '',
+        'BarcodeA' => '',
+        'BarcodeB' => '',
+        'BarcodeC' => '',
+        'PostBarcodeA' => '',
+        'PostBarcodeB' => '',
+        'PostBarcodeC' => '',
+        'EntityATM' => '91708888888888',
+        'BankCode' => '8220163',
+        'BankName' => '中國信託銀行 敦南分行',
+        'CargoNo' => '',
+        'StoreName' => '',
+        'StoreID' => '',
+        'InvoiceNo' => '',
+        'tokenData' => '',
+        'ChkValue' => 'C0A61FA4830F0B171273B2DC0CCFA2A9BA719A76',
+    ];
+
+    public function test_constructable()
+    {
+        $this->assertInstanceOf(BankTransfer::class, new BankTransfer($this->makeRequest($this->parameters), 'abcd5888'));
+    }
+}
