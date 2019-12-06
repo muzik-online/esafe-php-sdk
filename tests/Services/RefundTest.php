b9a0beb948d99cb84a38837f484ede307d4af701
Vincent Chi <song374561@chivincent.net>
2019-12-06 11:20:06 +0800

    test: create test case for send failed when refunding.

--- tests/Services/RefundTest.php
+++ tests/Services/RefundTest.php
@@ -2,6 +2,13 @@
 
 namespace Test\Services;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Muzik\EsafeSdk\Exceptions\HandlerException;
 use PHPUnit\Framework\TestCase;
 use Muzik\EsafeSdk\Services\RefundService;
 use Muzik\EsafeSdk\Exceptions\RefundException;
@@ -35,5 +42,41 @@ class RefundTest extends TestCase
             'Td' => '',
             'RefundMemo' => 'Hello World',
         ], 'abcd5888');
+    }
+
+    public function test_send_connection_failed()
+    {
+        $mock = new MockHandler([
+            new RequestException('Error Communicating with Server', new Request('post', 'http://esafe.test/'))
+        ]);
+        $client = new Client(['handler' => HandlerStack::create($mock)]);
+
+        $this->expectException(RefundException::class);
+        $service = new RefundService([
+            'web' => 'S1103020010',
+            'MN' => '1000',
+            'buysafeno' => '2400009912300000019',
+            'Td' => 'AC9087201',
+            'RefundMemo' => 'Hello World',
+        ], 'abcd5888', true, $client);
+        $service->send();
+    }
+
+    public function test_send_response_unexpected()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], '非授權使用位置.請與紅陽端提出申請')
+        ]);
+        $client = new Client(['handler' => HandlerStack::create($mock)]);
+
+        $this->expectException(RefundException::class);
+        $service = new RefundService([
+            'web' => 'S1103020010',
+            'MN' => '1000',
+            'buysafeno' => '2400009912300000019',
+            'Td' => 'AC9087201',
+            'RefundMemo' => 'Hello World',
+        ], 'abcd5888', true, $client);
+        $service->send();
     }
 }
